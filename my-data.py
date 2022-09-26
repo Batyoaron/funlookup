f520b47551aa18a04e56189fc4243c511104e9a1
batyoaron <111697446+Batyoaron@users.noreply.github.com>
2022-09-26 14:56:29 +0200

    Update my-data.py

--- my-data.py
+++ my-data.py
@@ -1,1 +1,9 @@
-
+import pyfiglet
+import os
+import time
+os.system('clear')
+print("\033[1;33;40m \n")
+ascii_banner = pyfiglet.figlet_format("funlookup")
+print(ascii_banner)
+print("\033[1;32;40m \n")
+print('my data')
