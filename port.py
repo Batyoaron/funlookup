86ffabc6e664ad6c6a3a5b439f0b9c6dfccbf04a
batyoaron <111697446+Batyoaron@users.noreply.github.com>
2022-09-26 19:42:04 +0200

    Update port.py

--- port.py
+++ port.py
@@ -1,10 +1,11 @@
 import pyfiglet
 import os
 import time
-os.system('clear')
 print("\033[1;33;40m \n")
 ascii_banner = pyfiglet.figlet_format("funlookup")
 print(ascii_banner)
+print("\033[1;31;40m \n")
+print('                                 [by]: Batyoaron')
 print("\033[1;32;40m \n")
 print('port')
 
