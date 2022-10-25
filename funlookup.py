12b37fe42dedb0f9b71ec06796a1c45f64f1440f
batyoaron <111697446+Batyoaron@users.noreply.github.com>
2022-10-25 15:04:27 +0200

    Update funlookup.py

--- funlookup.py
+++ funlookup.py
@@ -153,7 +153,7 @@ def print_packet(packet):
     # get the MAC address of the requester
     if packet.haslayer(Ether):
         target_mac = packet.getlayer(Ether).src
-    # get the DHCP options
+    
     dhcp_options = packet[DHCP].options
     for item in dhcp_options:
         try:
@@ -161,16 +161,16 @@ def print_packet(packet):
         except ValueError:
             continue
         if label == 'requested_addr':
-            # get the requested IP
+           
             requested_ip = value
         elif label == 'hostname':
-            # get the hostname of the device
+           
             hostname = value.decode()
         elif label == 'vendor_class_id':
-            # get the vendor ID
+            
             vendor_id = value.decode()
     if target_mac and vendor_id and hostname and requested_ip:
-        # if all variables are not None, print the device details
+        
         time_now = time.strftime("[%Y-%m-%d - %H:%M:%S]")
         print(f"{time_now} : {target_mac}  -  {hostname} / {vendor_id} requested {requested_ip}")
 
